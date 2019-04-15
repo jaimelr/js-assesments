78687f430e5eada4c45d5f09286017b94990c627
Jaime Loyola <j.loyolarangel@gmail.com>
2019-04-15 13:54:04 -0500

    Fix broken specs for numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    const binNum = num.toString(2);
+    return parseInt(binNum.charAt(binNum.length - bit));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2)
   },
 
   convertToBinary: function(num) {
-
+    const str = num.toString(2);
+    return str.padStart(8, '0');
   },
 
   multiply: function(a, b) {
-
+    const v = a * b;
+    return Number(v.toPrecision(4));
   }
 };
