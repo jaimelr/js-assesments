503b008eda1b5381c4dcdbb85670d9ab90894c90
Jaime Loyola <j.loyolarangel@gmail.com>
2019-05-28 18:34:43 -0500

    Fix broken specs for async

--- app/async.js
+++ app/async.js
@@ -2,10 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise((resolve, reject) => {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
-
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const arr = []
+        data.people.forEach(person => arr.push(person.name));
+        return arr.sort();
+      })
   }
 };
