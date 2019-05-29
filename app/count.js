e6a3ab464dd8cea34dc68854f600949ba03434fa
Jaime Loyola <j.loyolarangel@gmail.com>
2019-05-29 08:51:41 -0500

    Fix broken specs for counters

--- app/count.js
+++ app/count.js
@@ -2,6 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    var timer;
 
+    const printNumber = () => {
+      console.log(start);
+      start++;
+      if (start <= end) {
+        timer = setTimeout(printNumber, 100);
+      }
+    }
+
+    printNumber();
+
+    const control = {
+      cancel: function () {
+        clearTimeout(timer);
+      }
+    };
+
+    return control;
   }
 };
