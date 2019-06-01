f27d4ec1dcfefe54bf18405d79f3ab433b6c4ba8
Jaime Loyola <j.loyolarangel@gmail.com>
2019-06-01 17:00:56 -0500

    Fix broken specs for objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    return constructor.prototype.greeting = greeting
   },
 
   iterate: function(obj) {
+    let res = []
+    for(let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        res.push(key + ': ' + obj[key])
+      }
+    }
 
+    return res;
   }
 };
