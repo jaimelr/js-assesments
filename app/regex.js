741f0be93a77a552c71b3b9077c5726c9bf626a1
Jaime Loyola <j.loyolarangel@gmail.com>
2019-06-01 17:02:00 -0500

    Fix broken specs for regex

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,31 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return (/\d/).test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    return (/([A-Za-z])\1/).test(str);
   },
 
   endsWithVowel: function(str) {
-
+    return (/[aeiou]$/i).test(str);
   },
 
   captureThreeNumbers: function(str) {
+    const res = (/\d{3}/).exec(str);
+    if (res) {
+      return res[0]
+    }
 
+    return false;
   },
 
   matchesPattern: function(str) {
-
+    return (/^\d{3}-\d{3}-\d{4}$/).test(str);
   },
 
   isUSD: function(str) {
-
+    return (/^\$\d{1,3}(,\d{3})*(\.\d{2})?$/).test(str);
   }
 };
