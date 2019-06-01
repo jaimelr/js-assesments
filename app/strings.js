95317b7eeaf1966eef88818dca038e49f327a35e
Jaime Loyola <j.loyolarangel@gmail.com>
2019-06-01 17:02:24 -0500

    Fix broken specs for strings

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,27 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    var duplicatedChars = [];
+    var tmp = '';
 
+    function getDuplicatedChars(str, index) {
+      return str.filter(c => c == str[index])
+    }
+    for (let i = 0; i < str.length; i++) {
+      if (tmp === str[i]) {
+        continue;
+      }
+      tmp = str[i]
+      duplicatedChars.push(getDuplicatedChars(str.split(''), i))
+    }
+
+    var res = duplicatedChars.map(arr => {
+      while(arr.length > amount)
+        arr.pop()
+      return arr.join('');
+    });
+
+    return res.join('');
   },
 
   wordWrap: function(str, cols) {
@@ -10,6 +30,6 @@ exports.stringsAnswers = {
   },
 
   reverseString: function(str) {
-
+    return str.split('').reverse().join('');
   }
 };
